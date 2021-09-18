ca391131f5e85368651d534c03574620f951380b
Yash Chatim <yash.chatim@gmail.com>
2021-09-19 00:44:07 +0100

    Refactored App.js and added MainHeader component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
 import React, { Component } from "react";
-import "./App.css";
-import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
-import { Link } from "react-router-dom";
-import Main from "./components/main";
-import Pdf from "./cv.pdf"
+
+import MainHeader from "./components/Layout/MainHeader";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Layout>
-          <Header className="header-color" title={<Link to="/">
-            <a rel="noopener noreferrer" target="_blank">
-              <i className="fa fa-home" aria-hidden="true" />
-            </a></Link>}>
-
-            <div className="resume-container">
-              <a href={Pdf} target="_blank" rel="noopener noreferrer">View CV</a>
-              <a href="mailto:chatim.yash@yahoo.co.uk">Contact</a>
-            </div>
-          </Header>
-          <Drawer>
-            {/* Drawer - provide access to destinations in the app */}
-            <Navigation className="navbar">
-              <Link className="nav-1" to="/">Home</Link>
-              <Link className="nav-2" to="/aboutme">About</Link>
-              <Link className="nav-3" to="/projects">Projects</Link>
-              <a href="mailto:chatim.yash@yahoo.co.uk">Contact</a>
-              {/* Link - allow to navigate around the app by rendering fully accessible anchor tag with proper href */}
-            </Navigation>
-          </Drawer>
-          <Content>
-            <div className="page-content" />
-            <Main />
-          </Content>
-        </Layout>
+        <MainHeader />
       </div>
     );
   }
