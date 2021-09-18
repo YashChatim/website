4da3beacdca5f285d005e8f44d5b02d7902bec57
Yash Chatim <yash.chatim@gmail.com>
2021-09-19 00:48:05 +0100

    Imported React and Component

--- src/components/Layout/MainHeader.js
+++ src/components/Layout/MainHeader.js
@@ -1,3 +1,4 @@
+import React, { Component } from "react";
 import "../../App.css";
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import { Link } from "react-router-dom";
