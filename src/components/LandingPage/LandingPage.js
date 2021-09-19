5201b35360887e09eb87920b99d75a297d7d41b5
Yash Chatim <yash.chatim@gmail.com>
2021-09-19 03:29:41 +0100

    Refactored css for SocialLinks

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -21,10 +21,6 @@ class LandingPage extends Component {
                             <h2>D E V E L O P E R  /  P R O G R A M M E R</h2>
                             <hr /> {/* hr - thematic break in HTML e.g. topic change */}
                             <SocialLinks />
-                            <div className={classes['contact-info']}>
-                                <i className="fa fa-mobile" aria-hidden="true" /> (+44) 7507704668
-                                <i className="fa fa-envelope" aria-hidden="true" /> chatim.yash@yahoo.co.uk
-                            </div>
                         </div>
                     </Cell>
                 </Grid>
--- src/components/LandingPage/SocialLinks.js
+++ src/components/LandingPage/SocialLinks.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+import classes from './SocialLinks.module.css';
 
 const socialLinks = [
     {"href": "https://www.linkedin.com/in/yash-chatim-a2624b174/", "class": "fa fa-linkedin-square"},
@@ -10,13 +12,19 @@ const socialLinks = [
 class SocialLinks extends Component {
     render() {
         return (
-            <div className="social-links"> {/* target - specifies where to open the link, _blank - 	opens link in new window/tab */}
-                {socialLinks.map((item) =>
-                    <a href={item.href} rel="noopener noreferrer" target="_blank">
-                        <i class={item.class} aria-hidden="true"></i>
-                    </a>
-                )}
-            </div>
+            <Fragment>
+                <div className={classes['social-links']}> {/* target - specifies where to open the link, _blank - 	opens link in new window/tab */}
+                    {socialLinks.map((item) =>
+                        <a href={item.href} rel="noopener noreferrer" target="_blank">
+                            <i class={item.class} aria-hidden="true"></i>
+                        </a>
+                    )}
+                </div>
+                <div className={classes['contact-info']}>
+                    <i className="fa fa-mobile" aria-hidden="true" /> (+44) 7507704668
+                    <i className="fa fa-envelope" aria-hidden="true" /> chatim.yash@yahoo.co.uk
+                </div>
+            </Fragment>
         );
     }
 }
