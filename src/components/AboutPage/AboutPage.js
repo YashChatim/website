e412b15c180db01516376c316c23bdee3cf07e80
Yash Chatim <yash.chatim@gmail.com>
2021-09-23 22:00:54 +0100

    Swapped Education and FrontendSkills

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -9,59 +9,55 @@ import OtherSkills from './OtherSkills';
 import classes from './AboutPage.module.css';
 
 const educations = [
-  {"instituteName": "The University of Sheffield", "courseName": "BEng - Aerospace Engineering", "startYear": 2015, "endYear": 2018},
-  {"instituteName": "Wembley High Technology College", "courseName": "Mathematics, Physics, Further mathematics and IT", "startYear": 2013, "endYear": 2015},
+    {"instituteName": "The University of Sheffield", "courseName": "BEng - Aerospace Engineering", "startYear": 2015, "endYear": 2018},
+    {"instituteName": "Wembley High Technology College", "courseName": "Mathematics, Physics, Further mathematics and IT", "startYear": 2013, "endYear": 2015},
 ]
 
 class About extends Component {
-  render() {
-    return (
-      <div className={classes['about-body']}>
-        <Grid className={classes['about-grid']}>
-          <Cell col={4}>
-            <div style={{ textAlign: "center" }}>
-              <img src="https://i.pinimg.com/originals/fc/4f/72/fc4f726b36792d548f77896de0568a3a.jpg"
-                alt="profile" className={classes['profile-pic']} />
+    render() {
+        return (
+            <div className={classes['about-body']}>
+                <Grid className={classes['about-grid']}>
+                    <Cell col={4}>
+                        <div style={{ textAlign: "center" }}>
+                            <img src="https://i.pinimg.com/originals/fc/4f/72/fc4f726b36792d548f77896de0568a3a.jpg"
+                                alt="profile" className={classes['profile-pic']} />
+                        </div>
+                    </Cell>
+
+                    <Cell col={8}>
+                        <div className={classes['statement-container']}>
+                            <p>I am a proficient frontend web developer with knowledge and competency in backend.
+                            I have 3 years of academic experience in Javascript as well as 2 years in python, C# and Matlab.
+                            Additionally, I have 2 years of self-assigned experience working with the entire frontend pipeline
+                            which I have demonstrated through independent projects. My passion for development makes me a selfsufficient
+                            candidate eager to adapt to up-coming technologies and as such, refining my knowledge and skills further through
+                            professional experience.</p>
+                        </div>
+                        <Grid>
+                            <Cell className="institute" col={7}>
+                                <h4>Education</h4>
+                                {educations.map((item) => <Education instituteName={item.instituteName} courseName={item.courseName} startYear={item.startYear} endYear={item.endYear} />)}
+                            </Cell>
+                            <Cell col={5}>
+                                <h4>Technical Skills</h4>
+                                <FrontendSkills />
+                            </Cell>
+                        </Grid>
+                        <Grid>
+                            <Cell col={5}>
+                                <BackendSkills />                
+                            </Cell>
+
+                            <Cell col={5}>
+                                <OtherSkills />
+                            </Cell>
+                        </Grid>
+                    </Cell>
+                </Grid>
             </div>
-          </Cell>
-
-          <Cell col={8}>
-            <div className={classes['statement-container']}>
-              <p>I am a proficient frontend web developer with knowledge and competency in backend.
-              I have 3 years of academic experience in Javascript as well as 2 years in python, C# and Matlab.
-              Additionally, I have 2 years of self-assigned experience working with the entire frontend pipeline
-              which I have demonstrated through independent projects. My passion for development makes me a selfsufficient
-              candidate eager to adapt to up-coming technologies and as such, refining my knowledge and skills further through
-              professional experience.</p>
-            </div>
-
-            <Grid>
-              <Cell col={5}>
-                <h4>Technical Skills</h4>
-                <FrontendSkills />
-              </Cell>
-
-              <Cell className="institute" col={7}>
-                <h4>Education</h4>
-                {educations.map((item) => <Education instituteName={item.instituteName} courseName={item.courseName} startYear={item.startYear} endYear={item.endYear} />)}
-              </Cell>
-            </Grid>
-
-            <Grid>
-              <Cell col={5}>
-                <BackendSkills />                
-              </Cell>
-
-              <Cell col={5}>
-                <OtherSkills />
-              </Cell>
-
-            </Grid>
-          </Cell>
-        </Grid>
-      </div >
-    );
-  }
+        );
+    }
 }
 
 export default About;
